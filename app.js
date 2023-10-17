adb55773b6df6793828ccf283fb1363aff97c05d
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2023-10-17 08:03:16 +0300

    added invalid route middleware

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const response = require("./interceptors/response.interceptor");
 dotenv.config();
 
 app.use(express.json());
+const APP_PORT = process.env.APP_PORT || 3001;
 
 // Routes
 app.use("/users", require("./routes/user.route"));
 
-const APP_PORT = process.env.APP_PORT || 3001;
+// For Invalid Routes
+app.use((req, res, next) => {
+  response.badRequest(res, "Invalid path", 404);
+});
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port ${APP_PORT}`);
