088d119bfb8de703dedb416b2af5eaa4c34324f0
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2023-11-18 21:52:44 +0200

    sql update

--- sdl_application.sql
+++ sdl_application.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Nov 18, 2023 at 02:32 AM
+-- Generation Time: Nov 18, 2023 at 07:52 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.19
 
@@ -61,7 +61,8 @@ CREATE TABLE `jobs` (
 
 INSERT INTO `jobs` (`id`, `user_id`, `category_id`, `description`, `country`, `is_active`, `show_fee`, `is_deleted`, `created_at`, `updated_at`) VALUES
 (1, 15, 1, 'Test iş ilanı', 'RM 177', 1, 0, 0, '2023-11-18 00:14:03', NULL),
-(3, 15, 1, 'Lorem ipsum set amet dolor', 'RM 177', 0, 0, 0, '2023-11-18 01:07:35', '2023-11-18 01:07:35');
+(3, 15, 1, 'Lorem ipsum set amet dolor', 'RM 177', 0, 0, 0, '2023-11-18 01:07:35', '2023-11-18 01:07:35'),
+(4, 18, 1, 'Lorem ipsum set amet dolor', 'RM 177', 0, 0, 0, '2023-11-18 17:32:08', '2023-11-18 17:32:08');
 
 -- --------------------------------------------------------
 
@@ -154,8 +155,8 @@ CREATE TABLE `listings_include_files` (
 
 INSERT INTO `listings_include_files` (`id`, `listing_id`, `file_id`, `created_at`, `updated_at`) VALUES
 (1, 8, 2, '2023-11-13 21:43:29', '2023-11-13 21:43:29'),
-(2, 8, 3, '2023-11-13 21:43:29', '2023-11-13 21:43:29'),
-(3, 8, 5, '2023-11-13 21:43:29', '2023-11-13 21:43:29');
+(2, 5, 3, '2023-11-13 21:43:29', '2023-11-13 21:43:29'),
+(3, 5, 5, '2023-11-13 21:43:29', '2023-11-13 21:43:29');
 
 -- --------------------------------------------------------
 
@@ -181,9 +182,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `phone`, `country_code`, `password`, `email`, `is_active`, `created_at`, `updated_at`) VALUES
-(15, 'Emre', 'Kara', '5338350024', '90', '$2b$10$A3K/Mrgo7PJsp8OlQnESVOq0iV1rUuhvg70eiHtC3C1vOe9q9NpQ2', 'mhcifci@gmail2.com', 1, '2023-11-03 16:56:43', '2023-11-17 22:30:24'),
+(15, 'Emre', 'Kara', '5338350024', '90', '$2b$10$xceFVgSl3W.jLCbaRJ.ZIurO5wrPq6N5Gsx521Ms5y03OtoJ6vgkG', 'mhcifci@gmail2.com', 1, '2023-11-03 16:56:43', '2023-11-18 19:37:39'),
 (16, 'Emre', 'Kara', '5338350025', '90', '$2b$10$Pp2Qp11wu6Uj4zSDyXWIruOd.KaoVTmfAaICHyg8P4nBWzBGHt7L6', 'mhcifci@gmail3.com', 1, '2023-11-13 23:04:19', '2023-11-13 23:04:19'),
-(17, 'Yeni', 'Kullanıcı', '5338350026', '90', '$2b$10$5Clqbho3hEVQeWGIeHDbluqJ0m0qdl.5XfRFLAWu9k.dzOw0a.tjm', 'apply.cifci@gmail.com', 1, '2023-11-13 23:14:17', '2023-11-17 23:09:20'),
+(17, 'Yeni', 'Kullanıcı', '5338350026', '90', '$2b$10$R9JQ1KJWgcZCHmuSXMD8aeIFKNOHtGlsG8LyjZVf97KgFT1X49R3S', 'apply.cifci@gmail.com', 1, '2023-11-13 23:14:17', '2023-11-18 16:14:02'),
 (18, 'Yeni', 'Kullanıcı', '5338350028', '90', '$2b$10$AlD7OQ4ZMmgKGzSKqMFPxeQqPcTtQyPqqYPgH4uDmpZhFQ.I3B4s6', 'apply.cifci44@gmail.com', 1, '2023-11-17 22:16:04', '2023-11-17 22:16:04');
 
 -- --------------------------------------------------------
@@ -203,7 +204,7 @@ CREATE TABLE `user_details` (
 --
 
 INSERT INTO `user_details` (`id`, `user_id`, `img_id`) VALUES
-(1, 15, 17);
+(1, 15, 22);
 
 -- --------------------------------------------------------
 
@@ -224,7 +225,7 @@ CREATE TABLE `user_fcm_tokens` (
 --
 
 INSERT INTO `user_fcm_tokens` (`id`, `token`, `user_id`, `created_at`, `updated_at`) VALUES
-(2, 'qwewqe55', 15, '2023-11-03 18:24:21', '2023-11-17 21:57:54');
+(2, 'yeni', 15, '2023-11-03 18:24:21', '2023-11-18 16:16:10');
 
 -- --------------------------------------------------------
 
@@ -247,7 +248,8 @@ CREATE TABLE `user_follow_listings` (
 
 INSERT INTO `user_follow_listings` (`id`, `user_id`, `listing_id`, `is_following`, `created_at`, `updated_at`) VALUES
 (1, 16, 5, 1, '2023-11-18 02:00:32', NULL),
-(2, 16, 3, 0, '2023-11-18 02:00:32', NULL);
+(3, 15, 5, 0, '2023-11-18 02:00:32', NULL),
+(12, 18, 5, 1, '2023-11-18 19:27:53', '2023-11-18 19:27:58');
 
 -- --------------------------------------------------------
 
@@ -269,7 +271,9 @@ CREATE TABLE `user_lost_passwords` (
 --
 
 INSERT INTO `user_lost_passwords` (`id`, `user_id`, `code`, `is_used`, `created_at`, `updated_at`) VALUES
-(7, 17, '601555', 1, '2023-11-17 23:08:47', '2023-11-17 23:09:20');
+(7, 17, '601555', 1, '2023-11-17 23:08:47', '2023-11-17 23:09:20'),
+(8, 17, '890597', 1, '2023-11-18 16:11:32', '2023-11-18 16:14:02'),
+(9, 15, '800463', 1, '2023-11-18 19:37:22', '2023-11-18 19:37:39');
 
 -- --------------------------------------------------------
 
@@ -292,7 +296,8 @@ CREATE TABLE `user_opened_jobs` (
 
 INSERT INTO `user_opened_jobs` (`id`, `user_id`, `transaction_id`, `job_id`, `created_at`, `updated_at`) VALUES
 (30, 17, 34, 1, '2023-11-18 00:38:33', '2023-11-18 00:38:33'),
-(31, 16, 35, 1, '2023-11-18 00:43:00', '2023-11-18 00:43:00');
+(31, 16, 35, 1, '2023-11-18 00:43:00', '2023-11-18 00:43:00'),
+(32, 18, 36, 1, '2023-11-18 16:30:12', '2023-11-18 16:30:12');
 
 -- --------------------------------------------------------
 
@@ -314,10 +319,7 @@ CREATE TABLE `user_opened_listings` (
 --
 
 INSERT INTO `user_opened_listings` (`id`, `user_id`, `transaction_id`, `listing_id`, `created_at`, `updated_at`) VALUES
-(24, 16, 30, 4, '2023-11-13 23:08:37', '2023-11-13 23:08:37'),
-(25, 16, 31, 5, '2023-11-13 23:08:52', '2023-11-13 23:08:52'),
-(28, 18, 32, 3, '2023-11-13 23:12:44', '2023-11-13 23:12:44'),
-(29, 20, 33, 3, '2023-11-13 23:12:50', '2023-11-13 23:12:50');
+(31, 18, 37, 5, '2023-11-18 16:34:33', '2023-11-18 16:34:33');
 
 -- --------------------------------------------------------
 
@@ -339,13 +341,8 @@ CREATE TABLE `user_transactions` (
 --
 
 INSERT INTO `user_transactions` (`id`, `user_id`, `amount`, `reason`, `created_at`, `updated_at`) VALUES
-(28, 16, 1000, 'test', '2023-11-13 23:07:42', '2023-11-13 23:07:42'),
-(30, 16, -8, 'Listing View Fee #4', '2023-11-13 23:08:37', '2023-11-13 23:08:37'),
-(31, 16, -8, 'Listing View Fee #5', '2023-11-13 23:08:52', '2023-11-13 23:08:52'),
-(32, 16, -8, 'Listing View Fee #3', '2023-11-13 23:08:53', '2023-11-13 23:08:53'),
-(33, 16, -8, 'Listing View Fee #3', '2023-11-13 23:12:50', '2023-11-13 23:12:50'),
-(34, 17, 0, 'Jobs View Fee #1', '2023-11-18 00:38:33', '2023-11-18 00:38:33'),
-(35, 16, 0, 'Jobs View Fee #1', '2023-11-18 00:43:00', '2023-11-18 00:43:00');
+(28, 18, 1000, 'test', '2023-11-13 23:07:42', '2023-11-13 23:07:42'),
+(37, 18, -8, 'Listing View Fee #5', '2023-11-18 16:34:33', '2023-11-18 16:34:33');
 
 -- --------------------------------------------------------
 
@@ -368,9 +365,9 @@ CREATE TABLE `user_uploaded_files` (
 
 INSERT INTO `user_uploaded_files` (`id`, `user_id`, `file_url`, `description`, `created_at`, `updated_at`) VALUES
 (2, 15, '15_4bc6a410-cd92-4ad7-9617-1445bd72bbd4.png', NULL, '2023-11-09 18:07:18', '2023-11-09 18:07:18'),
-(3, 15, '15_5ef9ec05-a73d-4155-b83e-ee702723e86b.png', NULL, '2023-11-09 18:07:21', '2023-11-09 18:07:21'),
+(3, 15, '15_1a6af5f1-bc83-495a-b436-307a0bae6e43.png', NULL, '2023-11-09 18:07:21', '2023-11-09 18:07:21'),
 (4, 15, '15_93a4eeeb-cb75-4a1d-973e-1f2a9250b45b.png', NULL, '2023-11-09 18:07:22', '2023-11-09 18:07:22'),
-(5, 15, '15_b3feafc3-9389-4130-8d8f-f92010ab8bf3.png', NULL, '2023-11-09 18:07:23', '2023-11-09 18:07:23'),
+(5, 15, '15_1a6af5f1-bc83-495a-b436-307a0bae6e43.png', NULL, '2023-11-09 18:07:23', '2023-11-09 18:07:23'),
 (10, 15, '15_7d1f48cb-f6e9-40bb-8819-8bb2ec5f15df.png', 'Profile picture.', '2023-11-14 17:56:17', '2023-11-14 17:56:17'),
 (11, 15, '15_25e1875f-99ee-448b-a9ee-c3eb6845cdb3.png', 'Profile picture.', '2023-11-15 16:52:28', '2023-11-15 16:52:28'),
 (12, 15, '15_e0dcb62a-bc45-4f72-90c2-2c689c9915da.png', 'Profile picture.', '2023-11-17 21:42:04', '2023-11-17 21:42:04'),
@@ -378,7 +375,12 @@ INSERT INTO `user_uploaded_files` (`id`, `user_id`, `file_url`, `description`, `
 (14, 15, '15_d7144c48-6390-44a7-aa09-9b2a6b25cd9d.png', 'Profile picture.', '2023-11-17 21:49:50', '2023-11-17 21:49:50'),
 (15, 15, '15_29eba462-d044-4ab4-b077-c7c9b65ea644.png', 'Profile picture.', '2023-11-17 21:50:26', '2023-11-17 21:50:26'),
 (16, 15, '15_a821f64e-2d93-4891-9267-b0935a30a011.png', 'Profile picture.', '2023-11-17 21:52:02', '2023-11-17 21:52:02'),
-(17, 15, '15_eb14902b-df50-45ff-85a9-a35abb790489.png', 'Profile picture.', '2023-11-17 21:57:19', '2023-11-17 21:57:19');
+(17, 15, '15_eb14902b-df50-45ff-85a9-a35abb790489.png', 'Profile picture.', '2023-11-17 21:57:19', '2023-11-17 21:57:19'),
+(18, 15, '15_906337a8-54f1-4cf7-88c8-67530735457a.png', NULL, '2023-11-18 16:22:09', '2023-11-18 16:22:09'),
+(19, 15, '15_ce1adf54-42ec-4346-af00-13028a33ab42.png', NULL, '2023-11-18 16:22:31', '2023-11-18 16:22:31'),
+(20, 15, '15_ff0aa585-3fc1-446e-9231-86a9a1bbff19.png', NULL, '2023-11-18 16:22:34', '2023-11-18 16:22:34'),
+(21, 15, '15_1a6af5f1-bc83-495a-b436-307a0bae6e43.png', 'Profile picture.', '2023-11-18 16:56:09', '2023-11-18 16:56:09'),
+(22, 15, '15_02ac0d53-860d-45d4-9076-6096a2941079.png', 'Profile picture.', '2023-11-18 19:44:02', '2023-11-18 19:44:02');
 
 --
 -- Indexes for dumped tables
@@ -489,7 +491,7 @@ ALTER TABLE `companies`
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `jobs_categories`
@@ -537,37 +539,37 @@ ALTER TABLE `user_fcm_tokens`
 -- AUTO_INCREMENT for table `user_follow_listings`
 --
 ALTER TABLE `user_follow_listings`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `user_lost_passwords`
 --
 ALTER TABLE `user_lost_passwords`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_opened_jobs`
 --
 ALTER TABLE `user_opened_jobs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `user_opened_listings`
 --
 ALTER TABLE `user_opened_listings`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `user_transactions`
 --
 ALTER TABLE `user_transactions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `user_uploaded_files`
 --
 ALTER TABLE `user_uploaded_files`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
